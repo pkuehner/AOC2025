e56c25be62c635289eddf8643f2a3b2c69ad7d7e
Pascal Kühner <pascal.kuehner@tgr-europe.com>
2025-12-11 10:48:54 +0100

    Lets go

--- day11/solution.py
+++ day11/solution.py
@@ -12,33 +12,41 @@ def parse_input(lines):
     return result
 
 
-cache = {"dac": {}, "fft": {}, "both": {}, "none": {}}
-cache_paths = {}
-sol_cnt = 0
+cache = {} # Stores tuple (none, dac, fft, both)
 
 def solve_part_2(lines):
     hs = parse_input(lines)
-    count_paths(hs, "svr", set())
-    return sol_cnt
+    _, _, _, both_cnt = count_paths(hs, "svr", set())
+    return both_cnt
 
 
 def count_paths(hs, current, path):
-    my_cnt = 0
-    for nb in hs[current]:
-        if nb not in path:
-            if nb == "out":
-                if "fft" in path and "dac" in path:
-                    my_cnt += 1
-            elif nb in cache:
-                my_cnt += cache[nb]
-            else:
-                new_path_set = path.copy()
-                new_path_set.add(nb)
-                my_cnt += count_paths(hs, nb, new_path_set)
-    cache[current] = my_cnt 
-    if "dac" in path and "fft" in path:
-        global sol_cnt
-        sol_cnt += my_cnt                         
-    return my_cnt
+    if current == "out":
+        return 1, 0, 0, 0
+    
+    if current in cache:
+        return cache[current]
+    
+    # Four counters for paths starting from here
+    c_none, c_dac, c_fft, c_both = 0, 0, 0, 0
+    
+    if current in hs:
+        for nb in hs[current]:
+            if nb not in path:
+                r_none, r_dac, r_fft, r_both = count_paths(hs, nb, path | {current})
+                c_none += r_none
+                c_dac += r_dac
+                c_fft += r_fft
+                c_both += r_both
+
+    if current == "dac":
+        res = (0, c_none + c_dac, 0, c_fft + c_both)
+    elif current == "fft":
+        res = (0, 0, c_none + c_fft, c_dac + c_both)
+    else:
+        res = (c_none, c_dac, c_fft, c_both)
+        
+    cache[current] = res
+    return res
                 
-print(solve_part_2(read_lines("input_test_2")))
+print(solve_part_2(read_lines("input_1")))
